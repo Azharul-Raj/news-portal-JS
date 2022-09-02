c52f613f06928dcbb1ef3c0857ec14f6fa1deee0
Azharul-Raj <azharulislam.raj512@gmail.com>
2022-09-03 00:03:24 +0600

    dynamic card added

--- app.js
+++ app.js
@@ -38,9 +38,47 @@ const loadBlogs = async (id) => {
 }
 // display blogs part
 const displayBlogs = async (blogs) => {
+  const blogList = document.getElementById('blog-list')
+  blogList.textContent = ''
     blogs.forEach(blog => {
         console.log(blog)
         const { image_url, title, details, author, total_view, _id, thumbnail_url } = blog
-        const {img,name,published_date}= author
+        const { img, name, published_date } = author
+        const div = document.createElement('div')
+        div.innerHTML = `
+        <div class="card mb-3" style="max-width: 1040px;">
+        <div class="row g-0">
+          <div class="col-md-4">
+            <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">${title}</h5>
+              <p class="card-text">${details.length > 200? details.slice(0,200)+'....': details}</p>
+                <!-- card footer part start -->
+                <div class="row">
+                   <div class="col-lg-4">
+                      <!-- author details showing part start -->
+                      <div class="row">
+                        <div class="col-md-2">
+                          <img style="height:30px; width:30px" class="rounded-circle" src="${img}" alt="">
+                        </div>
+                        <div class="col-md-10">
+                         <p class="fw-bold">${name? name:'no name found'}</p>
+                         <p class="date">${published_date? published_date: 'date is not avilable'}</p>
+                        </div>
+                      </div>
+                      <!-- author details showing part end -->
+                   </div>
+                    <div class="col-lg-4">2</div>
+                    <div class="col-lg-4 justify-content-end">3</div>
+                </div>
+                <!-- card footer part end -->
+            </div>
+          </div>
+        </div>
+      </div>
+        `
+        blogList.appendChild(div)
     })
 }
